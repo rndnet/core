7fc586231152e4594d5315c2569dd1c6d56815b0
rndnet rndnet <rndnet@epyc-host002.rndnet.net>
2020-10-22 09:32:57 +0300

    Fix kube-shell.sh

--- kube-shell.sh
+++ kube-shell.sh
@@ -1,2 +1,5 @@
 . common
-kubectl exec --stdin --tty ${POD_NAME} -- /bin/bash
+#kubectl exec --stdin --tty ${POD_NAME} -- /bin/bash
+
+n=$(kubectl get pods | grep rndnet-scheduler | awk '{print $1}')
+kubectl exec --stdin --tty $n -- /bin/bash
