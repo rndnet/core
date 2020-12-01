0a578be5adde44229158f96c88e2a37d8f87f92f
rndnet rndnet <rndnet@epyc-host002.rndnet.net>
2020-12-01 14:09:24 +0300

    Add helm params examples

--- helm/install-dry-test.sh
+++ helm/install-dry-test.sh
@@ -6,7 +6,14 @@ helm install -name $name  --dry-run --debug -n $ns ./rndnet-scheduler \
  --set scheduler.params="{docker_image=rndnet/python-bfj:latest,gpu_vendor=nvidia}" \
  --set-file scheduler.key=rndnet_scheduler.key
 
-#  --set resources.limits.memory='16Gi' \
-#  --set resources.limits.cpu=20 \
-#  --set resources.requests.memory='2Gi' \
-#  --set resources.requests.cpu=10 \ 
+# --set resources.limits.memory='16Gi' \
+# --set resources.limits.cpu=20 \
+# --set resources.requests.memory='2Gi' \
+# --set resources.requests.cpu=10 \ 
+
+# --set scheduler.params="" \
+# --set serviceAccount.create=false \
+# --set scheduler.type="mp" \
+# --set scheduler.command="{'your_command'}" \
+# --set image.repository="your_repository" \
+# --set image.tag="your-repository-tag" \
